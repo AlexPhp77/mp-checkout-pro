ceebba0b76b32594f115783b3d560c54e0f539cf
Alex R <alex.rai@hotmail.com>
2021-05-28 14:53:12 -0300

    Second commit

--- index.php
+++ index.php
@@ -1,20 +1,39 @@
 <?php 
 
-    require __DIR__ . "/vendor/autoload.php";
+    // SDK do Mercado Pago
+    require __DIR__ .  '/vendor/autoload.php';
+    // Adicione as credenciais
+    MercadoPago\SDK::setAccessToken('TEST-3977295609080069-071412-779f28b9bed5a39f27622e97e47fc4dd-252183771');
 
-    MercadoPago\SDK::setAccessToken("YOUR_ACCESS_TOKEN"); // Either Production or SandBox AccessToken
+    // Cria um objeto de preferência
+    $preference = new MercadoPago\Preference();
 
-    $payment = new MercadoPago\Payment();
-    
-    $payment->transaction_amount = 141;
-    $payment->token = "YOUR_CARD_TOKEN";
-    $payment->description = "Ergonomic Silk Shirt";
-    $payment->installments = 1;
-    $payment->payment_method_id = "visa";
-    $payment->payer = array(
-      "email" => "larue.nienow@email.com"
-    );
+    // Cria um item na preferência
+    $item = new MercadoPago\Item();
+    $item->title = 'Meu produto';
+    $item->quantity = 1;
+    $item->unit_price = 75.56;
+    $preference->items = array($item);
+    $preference->save();
+?>    
 
-    $payment->save();
+<script>
+// Adicione as credenciais do SDK
+  const mp = new MercadoPago('TEST-3a3bd445-16c1-4689-9ba7-1e5cbde652e6', {
+        locale: 'es-AR'
+  });
 
-    echo $payment->status;
+  // Inicialize o checkout
+  mp.checkout({
+      preference: {
+          id: 'YOUR_PREFERENCE_ID'
+      },
+      render: {
+            container: '.cho-container', // Indica onde o botão de pagamento será exibido
+            label: 'Pagar', // Muda o texto do botão de pagamento (opcional)
+      }
+});
+</script>
+
+// SDK MercadoPago.js V2
+<script src="https://sdk.mercadopago.com/js/v2"></script>
